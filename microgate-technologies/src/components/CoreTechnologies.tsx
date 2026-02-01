eaf194b1a0070aba2ce6c8ac7dc62952698080f8
sowmyakota-mv <mastersvisa.da31@gmail.com>
2026-02-01 13:59:48 +0530

    complete homepage

--- microgate-technologies/src/components/CoreTechnologies.tsx
+++ microgate-technologies/src/components/CoreTechnologies.tsx
@@ -183,7 +183,7 @@ const CoreTechnologies: React.FC = () => {
                       />
                       
                       {/* Image Navigation Dots */}
-                      <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex gap-2 z-20">
+                      {/* <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex gap-2 z-20">
                         {technologies.map((tech) => (
                           <button
                             key={tech.id}
@@ -200,7 +200,7 @@ const CoreTechnologies: React.FC = () => {
                             aria-label={`Show ${tech.title} image`}
                           />
                         ))}
-                      </div>
+                      </div> */}
                     </div>
                   ))}
                 </div>
